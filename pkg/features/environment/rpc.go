1c3f4fbed023e4e173a868b74506385c5fd6ba7c
Oliver Geiselhardt-Herms <ogh@deepl.com>
2026-03-27 08:54:26 +0100

    CC-2739: Env/PEM: Support non multi-re systems (QFX5220)

--- pkg/features/environment/rpc.go
+++ pkg/features/environment/rpc.go
@@ -5,8 +5,9 @@ package environment
 import "encoding/xml"
 
 type multiEngineResult struct {
-	XMLName xml.Name           `xml:"rpc-reply"`
-	Results multiEngineResults `xml:"multi-routing-engine-results"`
+	XMLName                         xml.Name                        `xml:"rpc-reply"`
+	Results                         multiEngineResults              `xml:"multi-routing-engine-results"`
+	EnvironmentComponentInformation environmentComponentInformation `xml:"environment-component-information"`
 }
 
 type multiEngineResults struct {
